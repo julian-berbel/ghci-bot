776a0b851320dc46320df76e806d3b3b1ce7c0bd
Julián Berbel Alt <julian@mumuki.org>
2021-03-07 14:24:09 -0300

    Ew, was indenting with tabs

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ require('dotenv').config()
 const client = new Discord.Client();
 
 function ghci(message, expression) {
-	eval_expression = /^\s*:/.test(expression) ? expression : `pp $ ${expression}`
-	exec.execFile('docker', ['exec', '-i', 'haskell', 'bash', '-c', 'timeout 5 ghci prettify.hs <<< $0', eval_expression],
-		(error, stdout, stderr) => {
-			var rta = stdout.split('\n')[3];
-			if (error) {
-				message.reply(error.toString());
-			} else {
-				message.reply(`Expresión evaluada:\n\`\`\`haskell\n ${stderr || rta}\`\`\``);
-			}
-		});
+  eval_expression = /^\s*:/.test(expression) ? expression : `pp $ ${expression}`
+  exec.execFile('docker', ['exec', '-i', 'haskell', 'bash', '-c', 'timeout 5 ghci prettify.hs <<< $0', eval_expression],
+    (error, stdout, stderr) => {
+      var rta = stdout.split('\n')[3];
+      if (error) {
+        message.reply(error.toString());
+      } else {
+        message.reply(`Expresión evaluada:\n\`\`\`haskell\n ${stderr || rta}\`\`\``);
+      }
+    });
 }
 
 client.on('message', message => {
-	if (message.content.startsWith('!ghci ')) {
-		var expression = message.content.replace(/^!ghci /, '');
-		ghci(message, expression);
-	}
+  if (message.content.startsWith('!ghci ')) {
+    var expression = message.content.replace(/^!ghci /, '');
+    ghci(message, expression);
+  }
 });
 
 client.login();
